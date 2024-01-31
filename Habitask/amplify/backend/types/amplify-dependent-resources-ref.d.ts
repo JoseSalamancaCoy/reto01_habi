13ab65dab8355d7c23ace97d1561a365c9a444b3
JoseSalamancaCoy <jose.salamancacoy@gmail.com>
2024-01-31 12:42:02 -0500

    Adiciona Amplify auth base

--- Habitask/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ Habitask/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -5,5 +5,16 @@ export type AmplifyDependentResourcesAttributes = {
       "GraphQLAPIIdOutput": "string",
       "GraphQLAPIKeyOutput": "string"
     }
+  },
+  "auth": {
+    "habitaskc5346d0b": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
   }
 }
